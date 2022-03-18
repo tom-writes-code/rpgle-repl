7914da3e82eb37227ecd7d12227572b558cef0bf
Martin Tosney <7196868+martintosney@users.noreply.github.com>
2022-03-18 13:00:12 +0000

    New build target to package REPL as a save file

--- Rules.mk
+++ Rules.mk
@@ -1,1 +1,28 @@
 SUBDIRS = cmd db dsp rpgle bnd 
+
+package:
+	$(call echo_cmd,"=== PACKAGE: Creating save file [$(OBJLIB)/RPGLEREPL]")
+	$(eval crtcmd := CRTSAVF FILE($(OBJLIB)/RPGLEREPL))
+	@$(PRESETUP);  \
+	launch "$(JOBLOGFILE)" "$(crtcmd)" >> $(LOGFILE) 2>&1 || true; \
+	$(POSTCLEANUP)
+	$(call echo_cmd,"=== PACKAGE: Granting public authority to library")
+	$(eval crtcmd := GRTOBJAUT OBJ($(OBJLIB)/*ALL) USER(*PUBLIC) OBJTYPE(*ALL) AUT(*ALL))
+	@$(PRESETUP);  \
+	launch "$(JOBLOGFILE)" "$(crtcmd)" >> $(LOGFILE) 2>&1 || true; \
+	$(POSTCLEANUP)
+	$(call echo_cmd,"=== PACKAGE: Granting public authority to objects")
+	$(eval crtcmd := GRTOBJAUT OBJ($(OBJLIB)/*ALL) USER(*PUBLIC) OBJTYPE(*ALL) AUT(*ALL))
+	@$(PRESETUP);  \
+	launch "$(JOBLOGFILE)" "$(crtcmd)" >> $(LOGFILE) 2>&1 || true; \
+	$(POSTCLEANUP)
+	$(call echo_cmd,"=== PACKAGE: Change ownership to QPGMR")
+	$(eval crtcmd := CHGOWN OBJ('/QSYS.LIB/$(OBJLIB).LIB') NEWOWN(QPGMR) SUBTREE(*ALL))
+	@$(PRESETUP);  \
+	launch "$(JOBLOGFILE)" "$(crtcmd)" >> $(LOGFILE) 2>&1 || true; \
+	$(POSTCLEANUP)
+	$(call echo_cmd,"=== PACKAGE: Saving objects to [$(OBJLIB)/RPGLEREPL]")
+	$(eval crtcmd := SAVOBJ OBJ(*ALL) LIB($(OBJLIB)) DEV(*SAVF) SAVF($(OBJLIB)/RPGLEREPL) CLEAR(*REPLACE) TGTRLS($(TGTRLS)) DTACPR(*YES) SELECT((*OMIT *ALL *FILE SAVF)))
+	@$(PRESETUP);  \
+	launch "$(JOBLOGFILE)" "$(crtcmd)" >> $(LOGFILE) 2>&1 || true; \
+	$(POSTCLEANUP)
